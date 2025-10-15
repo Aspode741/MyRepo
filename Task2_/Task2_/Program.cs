74870b36c8747a8e004890ed3630f8612437dc5e
Viacheslav.Sharov@urfu.me <Viacheslav.Sharov@urfu.me>
2025-10-15 22:38:57 +0500

    Правильный вариант

--- Task2_/Task2_/Program.cs
+++ Task2_/Task2_/Program.cs
@@ -10,27 +10,16 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите радиус нижнего основания: ");
-            var R = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите пятизначное число");
 
-            Console.WriteLine("Введите радиус верхнего основания: ");
-            var r = int.Parse(Console.ReadLine());
+            var n = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Введите высоту усеченного конуса: ");
-            var h = int.Parse(Console.ReadLine());
+            var pat = n % 10;
+            var tri = (n % 1000) / 100;
+            var odin = (n % 100000) / 10000;
 
-            
-            var volume = (1.0 / 3.0) * Math.PI * h * (R * R + R * r + r * r);
-
-            //образующfz
-            var l = Math.Sqrt(h * h + (R - r) * (R - r));
-
-            // S
-            var surfaceArea = Math.PI * (R * R + r * r + l * (R + r));
-
-
-            Console.WriteLine("Объем усеченного конуса: " + Math.Round(volume));
-            Console.WriteLine("Площадь поверхности: " + Math.Round(surfaceArea));
+            string result = string.Format("{0}{1}{2}", odin, pat, tri);
+            Console.WriteLine("Ваше число:" + result);
         }
     }
 }
