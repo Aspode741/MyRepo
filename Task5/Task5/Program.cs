4d43893ab90c8fdd231bdddc22ebd41128367f4c
Viacheslav.Sharov@urfu.me <Viacheslav.Sharov@urfu.me>
2025-10-15 23:48:26 +0500

    Task5

---
+++ Task5/Task5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = Calculate(2, 3) + Calculate(5, 7) + Calculate(11, 13);
+            Console.WriteLine(Math.Round(x, 3));
+        }
+
+        static double Calculate(double a, double b) =>
+            Math.Sqrt((1 + a * a) / b * b);
+
+    }
+}
