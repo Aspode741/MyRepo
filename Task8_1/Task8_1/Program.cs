e3fe62cc8d76084cca4ae18d4a0034f391f0e829
Viacheslav.Sharov@urfu.me <Viacheslav.Sharov@urfu.me>
2025-10-22 20:49:29 +0500

    Вложили код

--- Task8_1/Task8_1/Program.cs
+++ Task8_1/Task8_1/Program.cs
@@ -8,9 +8,20 @@ namespace Task8_1
 {
     internal class Program
     {
-        static void Main(string[] args)
+    static void Main(string[] args)
         {
-            var n = 5 + 4;
+            Console.WriteLine("Введитее число m");
+            var m = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число n");
+            var n = int.Parse(Console.ReadLine());
+
+            if (Qw(m, n))
+                Console.WriteLine("Только одно число из чисел m и n делится на 2, но не делится ни на 3, ни на 5");
+            else Console.WriteLine("Либо оба числа из чисел m и n не делятся на 2, но делится на 3 и на 5,"
+                + "либо оба из чисел m и n делятся на 2, но не делятся ни на 3, ни на 5");
         }
+        static bool Qw(int m, int n) =>
+            ((m % 2 == 0) && (m % 3 != 0) && (m % 5 != 0) && ((n % 2 != 0) || (n % 3 == 0) || (n % 5 == 0))) ||
+            ((n % 2 == 0) && (n % 3 != 0) && (n % 5 != 0) && ((m % 2 != 0) || (m % 3 == 0) || (m % 5 == 0)));
     }
 }
