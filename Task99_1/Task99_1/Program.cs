9ef906967fb521aef1674b21580c01cd0c76612c
Viacheslav.Sharov@urfu.me <Viacheslav.Sharov@urfu.me>
2025-10-29 18:49:42 +0500

    Нормальный код положили

--- Task99_1/Task99_1/Program.cs
+++ Task99_1/Task99_1/Program.cs
@@ -10,7 +10,18 @@ namespace Task99_1
     {
         static void Main(string[] args)
         {
-            var n = 0;
+            Console.WriteLine("Введите значение аргумента");
+
+            var x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"f({x:F2})={F(x):F2}");
+        }
+
+        static double F(double x)
+        {
+            if (x < -5) return 25 - x * x;
+            else if (x <= 5) return x * x - 25;
+            else return Math.Sqrt(x - 5);
         }
     }
 }
