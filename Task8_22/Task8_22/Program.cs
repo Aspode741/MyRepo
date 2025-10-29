0c26a7850f0b5c9ded6079ef66ffe6b834073caa
Viacheslav.Sharov@urfu.me <Viacheslav.Sharov@urfu.me>
2025-10-29 20:23:36 +0500

    Решили

--- Task8_22/Task8_22/Program.cs
+++ Task8_22/Task8_22/Program.cs
@@ -10,7 +10,20 @@ namespace Task8_22
     {
         static void Main(string[] args)
         {
-            var n = 0;
+            Console.WriteLine("Введите абсциссу точки");
+            var x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите ординату точки");
+            var y = double.Parse(Console.ReadLine());
+
+            if (IsInArea(x, y))
+                Console.WriteLine("Точка лежит в указанной области");
+            else
+                Console.WriteLine("Точка не лежит в указанной области");
+
         }
+
+        static bool IsInArea(double x, double y) =>
+            y >= 0 && x >= 2 || y <= -1 && x > 1;
+
     }
 }
