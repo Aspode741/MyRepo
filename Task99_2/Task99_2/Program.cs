da6c7f276d3920fc441eddbc25a9b2d30773cb7e
Viacheslav.Sharov@urfu.me <Viacheslav.Sharov@urfu.me>
2025-10-29 19:59:31 +0500

    Строчные буквы

--- Task99_2/Task99_2/Program.cs
+++ Task99_2/Task99_2/Program.cs
@@ -10,59 +10,36 @@ namespace Task99_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите год н. э.");
+            Console.WriteLine("Введите символ");
 
-            var year = int.Parse(Console.ReadLine());
+            var input = Console.ReadLine();
 
-            var yearInCycle = (year - 1984) % 60;
+            if (input.Length == 0)
+            {
+                Console.WriteLine("Символа нет :(");
+                return;
+            }
+            else if (input.Length > 1)
+            {
+                Console.WriteLine("Слишком много символов");
+                return;
+            }
 
-            if (yearInCycle < 0)
-                yearInCycle += 60;
+            var ch = input[0];
 
-            var animalNumber = yearInCycle % 12;
-            var colorNumber = yearInCycle / 12;
-
-            string color;
-
-            if (colorNumber == 0)
-                color = "зелен";
-            else if (colorNumber == 1)
-                color = "красн";
-            else if (colorNumber == 2)
-                color = "желт";
-            else if (colorNumber == 3)
-                color = "бел";
+            if (char.IsUpper(ch))
+                Console.WriteLine($"{ch} — заглавная буква");
+            else if (char.IsLower(ch))
+                Console.WriteLine($"{ch} — строчная буква");
+            else if (char.IsWhiteSpace(ch))
+                Console.WriteLine("Это пробел");
+            else if (char.IsDigit(ch))
+                Console.WriteLine($"{ch} — цифра");
+            else if (char.IsPunctuation(ch))
+                Console.WriteLine($"{ch} — знак пунктуации");
             else
-                color = "черн";
+                Console.WriteLine($"{ch} — специальный символ");
 
-            string animal;
-
-            if (animalNumber == 0)
-                animal = "ой крысы";
-            else if (animalNumber == 1)
-                animal = "ой коровы";
-            else if (animalNumber == 2)
-                animal = "ого тигра";
-            else if (animalNumber == 3)
-                animal = "ого зайца";
-            else if (animalNumber == 4)
-                animal = "ого дракона";
-            else if (animalNumber == 5)
-                animal = "ой змеи";
-            else if (animalNumber == 6)
-                animal = "ой лошади";
-            else if (animalNumber == 7)
-                animal = "ой овцы";
-            else if (animalNumber == 8)
-                animal = "ой обезьяны";
-            else if (animalNumber == 9)
-                animal = "ой курицы";
-            else if (animalNumber == 10)
-                animal = "ой собаки";
-            else
-                animal = "ой свиньи";
-
-            Console.WriteLine($"{year} г. — год {color}{animal}");
         }
     }
 }
