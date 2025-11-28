df32f38dea5459a38215a0da75506e86b5a47f82
Viacheslav.Sharov@urfu.me <Viacheslav.Sharov@urfu.me>
2025-11-28 23:27:34 +0500

    Задание 1

---
+++ Task10_1/Task10_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task24_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное число n");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите натуральное число k");
+            int k = int.Parse(Console.ReadLine());
+
+            double s = 1;
+            s += n;
+
+            for (int i = 2; i <= k; i++)
+            {
+                s += Math.Pow(n, 1.0 / i);
+            }
+
+            Console.WriteLine($"Сумма = {s:F5}");
+        }
+    }
+}
