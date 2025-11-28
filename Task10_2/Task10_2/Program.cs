7ef3e34c587200a6bd519c5549283045e749be17
Viacheslav.Sharov@urfu.me <Viacheslav.Sharov@urfu.me>
2025-11-28 23:34:20 +0500

    Задание 2

---
+++ Task10_2/Task10_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество чисел");
+            int n = int.Parse(Console.ReadLine());
+
+            double s = 0;
+            double p = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Введите {i + 1}-е число");
+                double x = double.Parse(Console.ReadLine());
+
+                p *= x;
+                s += p;
+            }
+
+            Console.WriteLine($"Результат = {s:F5}");
+        }
+    }
+}
